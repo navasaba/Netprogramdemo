adf85d6197f6b668641eff6b67d989bfc39bd24c
navasaba <navasaba@gmail.com>
2018-02-10 00:08:20 -0500

    This is an Exercise for EDX
    This is a tutorial for EDX

---
+++ untitled12.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 23 00:10:04 2018
+
+@author: NavaSaba
+"""
+
+x = 6
+if x != 5:
+     print("I am here")
+else:
+     print("I am not")
+     
